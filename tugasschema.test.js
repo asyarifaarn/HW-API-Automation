b6b0345fe6bf228f733122be652fede845c26a61
asyarifaarn <asyarifariskia@gmail.com>
2025-04-14 22:40:32 +0700

    Add files via upload

---
+++ tugasschema.test.js
@@ -0,0 +1,24 @@
+const schema_user baru = {
+    "type": "object",
+    "properties": {
+        "name": {
+            "type": "string"
+        },
+        "job": {
+            "type": "string"
+        },
+        "id": {
+            "type": "string"
+        },
+        "createsAt": {
+            "type": "string"
+        }
+    },
+    "required": [
+        "name",
+        "job",
+        "createdAt"
+    ]
+}
+
+export default schema_user baru;
